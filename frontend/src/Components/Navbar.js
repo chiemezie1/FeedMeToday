2ee548a9e467da951364670a2b70158c8a2c3a44
chiemezie1 <chiemezieagbo1@gmail.com>
2023-06-26 12:04:40 +0100

    added web3-react

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,13 +1,27 @@
 import navbar from "./Navbar.css";
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useWeb3React } from "@web3-react/core";
+import { InjectedConnector } from "@web3-react/injected-connector";
+
+export const injected = new InjectedConnector();
 
 function Navbar() {
   const [isConnected, setIsConnected] = useState(false);
+  const { active, activate, deactivate, chainId, account, library: provider } = useWeb3React();
 
-  const handleClick = () => {
-    setIsConnected(!isConnected);
-  };
+  async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        await activate(injected);
+        if (active && account) {
+          localStorage.setItem("connectedAccount", account);
+        }
+      } catch (e) {
+        console.log("Please install MetaMask" + e);
+      }
+    }
+  }
 
   return (
     <div className="navbar">
@@ -20,9 +34,10 @@ function Navbar() {
             <Link to="/about">About</Link>
           </li>
           <li>
-            <div className={`connect ${isConnected ? 'green' : 'red'}`} onClick={handleClick}>
+            <div className={`connect ${activate ? 'green' : 'red'}`} onClick={connect}>
               Connect
             </div>
+            <p>{account}</p>
           </li>
         </ul>
       </nav>
